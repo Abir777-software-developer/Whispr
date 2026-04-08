4b408a2b605834f481883f72291c804b955b5eff
Abir Dey <destroyer7362@gmail.com>
2026-04-09 03:32:11 +0530

    updated the socket connection,rate limiter,summarizer

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,11 @@ import express from "express";
 import { registerUser, authUser } from "../controllers/userControllers.js";
 import { allUsers } from "../controllers/userControllers.js";
 import { protect } from "../middleware/authmiddleware.js";
+import { authLimiter, searchLimiter } from "../middleware/rateLimitMiddleware.js";
 const router = express.Router();
 
-router.route("/").post(registerUser).get(protect, allUsers);
-router.post("/login", authUser);
+router.route("/").post(authLimiter, registerUser).get(protect, searchLimiter, allUsers);
+router.post("/login", authLimiter, authUser);
+
 
 export default router;
